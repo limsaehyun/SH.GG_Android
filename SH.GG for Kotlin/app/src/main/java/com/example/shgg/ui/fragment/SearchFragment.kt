d26e71c967a5557d252175c7802226a3c90b8b9a
limsaehyun <sh00710000@gmail.com>
2021-09-05 15:44:36 +0900

    Feat :: 프로젝트 생성 & binding 활성화

---
+++ SH.GG for Kotlin/app/src/main/java/com/example/shgg/ui/fragment/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.example.shgg.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.shgg.R
+import com.example.shgg.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        binding.textHome.setText("binding success")
+
+        return binding.root
+    }
+}
