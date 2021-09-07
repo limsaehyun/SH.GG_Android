ad5a44268e86d778a3ac56f18c2c0169d1415783
limsaehyun <sh00710000@gmail.com>
2021-09-07 14:36:37 +0900

    Refactor :: 패키지 정리

---
+++ SH.GG for Kotlin/app/src/main/java/com/example/shgg/ui/activity/EditNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shgg.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shgg.R
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
--- SH.GG for Kotlin/app/src/main/java/com/example/shgg/ui/activity/MainActivity.kt
+++ SH.GG for Kotlin/app/src/main/java/com/example/shgg/ui/activity/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.shgg
+package com.example.shgg.ui.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.shgg.R
 
 class MainActivity : AppCompatActivity() {
 
